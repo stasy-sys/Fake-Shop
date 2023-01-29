f31d1e8caa005bd3ed11207c274456a54814ca39
Anastasia Sys <stasy.sys@mail.ru>
2023-01-29 23:54:26 +0100

    feat: Added Products

---
+++ src/components/Product.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h4>
+            {{title}}
+        </h4>
+        {{ price }}$
+        <p>
+            {{ description }}
+        </p>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: {
+        title: String,
+        price: String,
+        description: String,
+    },
+});
+</script>
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,7 +1,39 @@
 <template>
-    <NavBar>
-        <h1>List of products en development</h1>
-    </NavBar>
+    <h1>List of products</h1>
+    <Product v-for="(item) in items" :title="item.title" :price="item.price" :description="item.description" />
 </template>
 
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import Product from '@/components/Product.vue';
+import fakeShopApi from '@/api/fakeShopApi';
+import { AxiosResponse } from 'axios';
+import { Item } from '@/interfaces/item';
 
+export default defineComponent({
+    // props: {
+    //     items: Item[],
+    // },
+    components: {
+        Product
+    },
+    setup() {
+        const productRequest = fakeShopApi.get<unknown, AxiosResponse<any>>(
+                `/products`
+        );
+        let items = ref<any>([]);
+        productRequest.then((response)=> {
+            response.data.forEach((element: any) => {
+                items.value.push({
+                    title: element.title,
+                    price: element.price,
+                    description: element.description,
+                })
+            });
+        })
+        return {
+            items,
+        }
+    }
+})
+</script>
