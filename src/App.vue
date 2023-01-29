7d6a12c2bdd801d1c2a87ad4c67d6c166186bdf2
Anastasia Sys <stasy.sys@mail.ru>
2023-01-29 20:00:00 +0100

    featCreated navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <NavBar shopName="Fake Shop"/>
+
   <router-view/>
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import NavBar from '@/components/NavBar.vue';
+
+export default defineComponent({
+  components: {NavBar},
+  setup() {
+    
+  },
+})
+</script>
 
 <style>
 #app {
@@ -12,19 +21,21 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #302c50;
 }
 
 nav {
   padding: 30px;
+
 }
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #000000;
+  text-decoration: none;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #6f8669;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="nav-bar">
+        <div class="logo">
+            <img class="logo-img" src="@/assets/logo.png" alt="">
+            <p class="logo-text">{{shopName}}</p>
+        </div>
+        <nav>
+            <router-link to="/products">Products</router-link> |
+            <router-link to="/profile">Profile</router-link>
+        </nav>
+        <button> Log out </button>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: {
+        shopName: String
+    },
+    setup() {
+        
+    }
+})
+</script>
+
+
+<style scoped>
+* { 
+    padding: 0;
+    margin: 0
+}
+.nav-bar {
+    background-color: rgb(166, 211, 142);
+    display: flex;
+    width: auto;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.5em;
+    
+}
+
+.logo {
+    display: flex;
+}
+.logo-img {
+    max-height: 2em;
+    align-self: center;
+}
+
+.logo-text {
+    font-size: large;
+    align-self: center;
+    text-shadow: 1px 1px 2px green;
+    color: white;
+}
+</style>
