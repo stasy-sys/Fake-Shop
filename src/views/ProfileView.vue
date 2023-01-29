da56efa2ade8434092da822c85374135d826c274
Anastasia Sys <stasy.sys@mail.ru>
2023-01-29 23:58:29 +0100

    feat:Addet Profile

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,8 +1,34 @@
 <template>
     <div>
-        <h1>Name</h1>
+        <h1>{{ name }}</h1>
         <img src="" alt="">
-        <h2>email</h2>
-        <h2>Rol</h2>
+        <h2>{{ email }}</h2>
+        <h2>{{ role }}</h2>
     </div>
 </template>
+
+<script lang="ts">import fakeShopApi from '@/api/fakeShopApi';
+import { AxiosResponse } from 'axios';
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+    setup() {
+        const productRequest = fakeShopApi.get<unknown, AxiosResponse<any>>(
+            `/auth/profile`
+        );
+        let name = ref<string>();
+        let email = ref<string>();
+        let role = ref<string>();
+
+        productRequest.then((response) => {
+            name.value = response.data.name;
+            email.value = response.data.email;
+            role.value = response.data.role;
+        })
+        return {
+            name,
+            email,
+            role,
+        }
+    }
+});
+</script>
